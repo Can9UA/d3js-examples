5226771373fa147002497028bb17516bbad3a4f4
alexandr.poddubnyi <alexandr.poddubnyi@ssa-outsourcing.com>
2016-10-30 23:45:19 +0200

    1. xScale added;

--- app/index.js
+++ app/index.js
@@ -30,8 +30,8 @@
 // ---------------------------
 // диограма из прямогуольников
 var barsData = [100, 50, 70, 140, 20];
-var barWidth = 20;
-var barOffset = 5;
+// var barWidth = 20;
+// var barOffset = 5;
 
 var canvas = d3.select('svg').style('background', '#c9d7d6');
 var canvasHeight = parseInt(canvas.attr('height'), 10);
@@ -41,18 +41,23 @@ var yScale = d3.scaleLinear() // for 4.x version
                .domain([0, d3.max(barsData)])
                .range([0, canvasHeight]);
 
+// var xScale = d3.scale.ordinal() // for 3.x
+var xScale = d3.scaleBand() // for 4.x version
+               .domain(d3.range(0, barsData.length))
+               .range([0, parseInt(canvas.node().clientWidth, 10)]);
+
 canvas.selectAll('rect')
       .data(barsData)
       .enter()
       .append('rect')
       .style('fill', '#c61c6f')
-      .attr('width', barWidth)
+      .attr('width', xScale.bandwidth()) // .attr('width', xScale.rangeBand()) for 3.x version
       .attr('height', function (d) {
         // return d; 
         return yScale(d); // scale height
       })
       .attr('x', function (d,i) {
-        return i * (barWidth + barOffset);
+        return xScale(i);
       })
       .attr('y', function (d) {
         return canvasHeight - yScale(d);
