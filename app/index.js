ec41a7684b403978f8cddbc5dd97feae9b89997c
alexandr.poddubnyi <alexandr.poddubnyi@ssa-outsourcing.com>
2016-10-31 00:20:51 +0200

    1. added colors for diagram;

--- app/index.js
+++ app/index.js
@@ -27,9 +27,10 @@
 //   })
 //   .style('color', 'white');
 
-// ---------------------------
+// --------------------------------------------
 // диограма из прямогуольников
-var barsData = [100, 50, 70, 140, 20];
+var barsData = [100, 50, 70, 140, 220,
+                100, 50, 70, 140, 220];
 // var barWidth = 20;
 // var barOffset = 5;
 
@@ -46,11 +47,33 @@ var xScale = d3.scaleBand() // for 4.x version
                .domain(d3.range(0, barsData.length))
                .range([0, parseInt(canvas.node().clientWidth, 10)]);
 
+// color depend of bar value ‡1
+// var colors = d3.scaleLinear()
+//                .domain([0, d3.max(barsData)])
+//                .range(['#ffb832', '#c61c6f']);
+
+// color depend of bar index position ‡2
+var colors = d3.scaleLinear()
+              .domain([0, barsData.length])
+              .range(['#ffb832', '#c61c6f']);
+
+// more colors
+// var colors = d3.scaleLinear()
+//   .domain([0, barsData.length * 0.33, barsData.length * 0.66, barsData.length])
+//   .range(['#b58929', '#c61c6f', '#268bd2', '#85992c']);
+
 canvas.selectAll('rect')
       .data(barsData)
       .enter()
       .append('rect')
-      .style('fill', '#c61c6f')
+      // color depend of bar value ‡1
+      // .style('fill', function (d) { // or just .style('fill', colors)
+      //   return colors(d);
+      // })
+      // color depend of bar index position ‡2
+      .style('fill', function (d, i) {
+        return colors(i);
+      })
       .attr('width', xScale.bandwidth()) // .attr('width', xScale.rangeBand()) for 3.x version
       .attr('height', function (d) {
         // return d; 
