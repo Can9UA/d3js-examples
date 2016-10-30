ec6849891332477c0c32e52e8d306882320bc6d2
alexandr.poddubnyi <alexandr.poddubnyi@ssa-outsourcing.com>
2016-10-30 22:22:45 +0200

    1. quantitative scale added;

--- app/index.js
+++ app/index.js
@@ -29,24 +29,32 @@
 
 // ---------------------------
 // диограма из прямогуольников
-// var barsData = [100, 50, 70, 40, 20];
-// var barWidth = 20;
-// var barOffset = 5;
+var barsData = [100, 50, 70, 140, 20];
+var barWidth = 20;
+var barOffset = 5;
 
-// var canvas = d3.select('svg').style('background', '#c9d7d6');
-// var canvasHeight = parseInt(canvas.attr('height'), 10);
+var canvas = d3.select('svg').style('background', '#c9d7d6');
+var canvasHeight = parseInt(canvas.attr('height'), 10);
 
-// canvas.selectAll('rect')
-//       .data(barsData)
-//       .enter()
-//       .append('rect')
-//       .style('fill', '#c61c6f')
-//       .attr('width', barWidth)
-//       .attr('height', function (d) { return d; })
-//       .attr('x', function (d,i) {
-//         return i * (barWidth + barOffset);
-//       })
-//       .attr('y', function (d) {
-//         return canvasHeight - d;
-//       });
+// var yScale = d3.scale.linear() // for 3.x version
+var yScale = d3.scaleLinear() // for 4.x version
+               .domain([0, d3.max(barsData)])
+               .range([0, canvasHeight]);
+
+canvas.selectAll('rect')
+      .data(barsData)
+      .enter()
+      .append('rect')
+      .style('fill', '#c61c6f')
+      .attr('width', barWidth)
+      .attr('height', function (d) {
+        // return d; 
+        return yScale(d); // scale height
+      })
+      .attr('x', function (d,i) {
+        return i * (barWidth + barOffset);
+      })
+      .attr('y', function (d) {
+        return canvasHeight - yScale(d);
+      });
 
